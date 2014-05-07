5c04255906e32418be1808a94f22f3d47a8f2e15
Jack <jack.willis-craig@hotmail.com>
2014-05-07 18:36:39 +1000

    Extended testing to cover more cases.

--- test/validate-all.js
+++ test/validate-all.js
@@ -16,16 +16,33 @@ describe('validate-all', function() {
       if(err) return done(err);
       
       user = new User({
-        username: 'invalid*character*and*too*long'
+        username: 'User'
       });
       done();
     });
   });
 
-  it('should return a list of error types in the message field when multiple errors exist', function(done) {
-    user.save(function(err) {
-      err.errors.username.message.should.containEql('LengthBelowMin') &&
+  it('should return a delimited list of errors in the message field when multiple errors exist', function(done) {
+    user.username = 'InvalidCharacter*TooLong';
+    user.validate(function(err) {
+      err.errors.username.message.should.containEql('LengthAboveMax') &&
         err.errors.username.message.should.containEql('InvalidCharacters');
+      done();
+    });
+  });
+  
+  it('should return a single error when one error exists', function(done) {
+    user.username = 'validCharactersTooLong';
+    user.validate(function(err) {
+      err.errors.username.message.should.eql('LengthAboveMax');
+      done();
+    });
+  });
+  
+  it('should not produce an error when no errors exist', function(done) {
+    user.username = 'User';
+    user.validate(function(err) {
+      should.not.exist(err);
       done();
     });
   });
